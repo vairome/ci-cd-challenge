689e492970d26d3e0d6752396de82ca02be3b993
Byron Murillo <bmurillo@gbm.net>
2022-05-16 16:59:20 -0600

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <h1>
-            CI/CD Challenge week 3 by Byron Murillo c: 
+            SEBAAAAS
           </h1>
         </p>
         
